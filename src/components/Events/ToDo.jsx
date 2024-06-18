4af6f1114b3e58be950819ac60f4c9d1a4e70d41
BIBEK7932 <bibekdas7932@gmail.com>
2024-06-18 10:32:17 +0530

    Todo Done

--- src/components/Events/ToDo.jsx
+++ src/components/Events/ToDo.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-import './ToDo.css'; // Import the CSS file
+import './ToDo.css'; 
 
 export default function ToDo() {
     let [toDo, setToDo] = useState([{ task: "", id: uuidv4() }]);
@@ -10,10 +10,23 @@ export default function ToDo() {
         if (newTask === "") { 
             alert("Enter a valid task");
         } else {
-            setToDo((prev) => [...prev, { task: newTask, id: uuidv4() }]);
+            setToDo((prev) =>{ return [...prev, { task: newTask, id: uuidv4() }]} );
             setNewTask("");
         }
     };
+
+let  deleteTask  = (id)=>{
+        let newtoDo = toDo.filter((data)=>{
+            return data.id != id
+        })
+        // console.log(toDo)
+        setToDo( (prev)=> prev.filter((data)=>{
+            return data.id != id
+        }))
+        // alert(id)
+        console.log(id)
+
+    }
 
     return (
         <div className="todo-container">
@@ -31,7 +44,11 @@ export default function ToDo() {
             <hr />
             <h4 className="todo-title">Tasks To Do</h4>
             <ul className="todo-list">
-                {toDo.map((t) => t.task !== "" && <li key={t.id} className="todo-item">{t.task}</li>)}
+                {toDo.map((t) => t.task !== "" && <li key={t.id} className="todo-item">
+                   <span>{t.task}</span> 
+                   &nbsp;
+                   <button className="doneButton" onClick={()=> deleteTask(t.id)}  >Done</button>
+                    </li>)}
             </ul>
         </div>
     );
